cb3f3c95955b32f279a1b69b5c8d360b212e400a
Elio Maisonneuve <elio.maisonneuve@eisti.fr>
2017-07-12 11:16:32 +0200

    initial project structure

---
+++ README.md
@@ -0,0 +1,4 @@
+Introduction
+============
+
+A nagios-like plugin to check if talend jobs executed successfully
---
+++ src/check_talend_job/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+check_talend_job
+------------------------------
+A nagios-like plugin to check if talend jobs executed successfully
+
+"""
+
+__docformat__ = 'restructuredtext en'
+
+# vim:set et sts=4 ts=4 tw=80:
